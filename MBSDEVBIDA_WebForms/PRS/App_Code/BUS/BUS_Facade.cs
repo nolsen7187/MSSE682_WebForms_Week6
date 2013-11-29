a42570c5168787503c24eae69d31e6915322f75d
nolsen7187 <nolsen@regis.edu>
2013-11-29 16:12:00 -0700

    Added DAL Tests

---
+++ MBSDEVBIDA_WebForms/PRS/App_Code/BUS/BUS_Facade.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRS.SVC;
+
+namespace PRS.BUS
+{
+    public class BUS_Facade
+    {
+        private SVC_PerformAction svcPerformAction; 
+        private SVC_AuthenticateUser svcAuthenticateUser;
+        private Object lclObjectClass;
+        private int lclActionType;
+
+        private String logon, password;
+        private bool foundLogon, foundPassword;
+
+        public BUS_Facade(Object Object, int ActionType)
+        {   
+            this.lclObjectClass = Object;
+            this.lclActionType = ActionType;
+        }
+        public BUS_Facade(string _Logon, string _Password = "")
+        {
+            this.logon = _Logon;
+            this.password = _Password;
+        }
+        public void ProcessRequest()
+        {
+            svcPerformAction = new SVC_PerformAction();
+            svcPerformAction.Action(lclObjectClass, lclActionType);
+        }
+        public string ProcessAuthenticationRequest()
+        {
+            svcAuthenticateUser = new SVC_AuthenticateUser();
+            foundLogon = svcAuthenticateUser.AuthenticateLogon(logon);
+            foundPassword = svcAuthenticateUser.AuthenticatePassword(password);
+            if (foundLogon && foundPassword)
+            {
+                return logon;
+            }
+            else
+            {
+                return "Invalid Password Entered";
+            }
+        }
+        public string ProcessLogonNameAuthenticity()
+        {
+            svcAuthenticateUser = new SVC_AuthenticateUser();
+            foundLogon = svcAuthenticateUser.AuthenticateLogon(logon);
+            if (foundLogon)
+            {
+                return logon;
+            }
+            else
+            {
+                return "Valid Username Entered.";
+            }
+        }
+    }
+}
